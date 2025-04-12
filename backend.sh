3f8a67d0ce8bf225e38e34a9eadb8a354a9cd220
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-04-12 17:48:20 +0530

    expense-backend

--- backend.sh
+++ backend.sh
@@ -65,7 +65,7 @@ VALIDATE $? "Extracting backend application code"
 
 # create backend.service file
 
-dnf install npm -y &>>$LOG_FILE
+npm install -g npm@11.3.0 &>>$LOG_FILE
 VALIDATE $? "Installing npm"
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
